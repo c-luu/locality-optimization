54f18e7d998f683090052fa9f268d472f0108179
cluu <cluu30@gatech.edu>
2018-11-27 23:21:34 -0500

    debug

--- README.md
+++ README.md
@@ -14,5 +14,8 @@ This project explores two compiler techniques for locality optimization at the f
 > ocamlc -o foo foo.ml
 
 ## TODO
-1. Front-end compiler
+1. Front-end compiler.
 2. CFG builder.
+
+## Resources
+1. [Lecture Slides](https://courses.cs.washington.edu/courses/csep501/18sp/lectures/S-valuenumbering_ink.pdf).
--- analysis.ml
+++ analysis.ml
@@ -3,7 +3,7 @@ type expression =
     | Symbol of string
     | Operation of binary_function * expression * expression;;
 type line = Line of expression * expression;;
-type basic_block = Lines of line list;;
+type basic_block = line list;;
 type ('k, 'v) lvn = ('k, 'v) Hashtbl.t;;
 
 (* CFG *)
@@ -16,58 +16,65 @@ type edge = node * node;;
 type edges = edge list;;
 type cfg = nodes * edges;;
 
-(* build_lvn : basic_block) ->  lvn;; *)
 (* path_exists : edge -> edge -> cfg -> bool;; *)
 
 (* Local Val Numbering *)
-
-(* Original block*)
 let a = Symbol "a";;
 let b = Symbol "b";;
+let x = Symbol "x";;
+let y = Symbol "y";;
 let c = Symbol "c";;
-let d = Symbol "d";;
+let seventeen = Symbol "17";;
 let add = BinaryFunction "+";;
 let sub = BinaryFunction "-";;
-let line_1 = Line (a, Operation (add, b, c));;
-let line_2 = Line (b, Operation (sub, a, d));;
-let line_3 = Line (c, Operation (add, b, c));;
-let line_4 = Line (d, Operation (add, a, d));;
-let blk_0 = Lines [ line_1; line_2; ];;
-let blk_1 = Lines [ line_1; line_2; line_3; line_4 ];;
+let line_1 = Line (a, Operation (add, x, y));;
+let line_2 = Line (b, Operation (add, x, y));;
+let line_3 = Line (a, seventeen);;
+let line_4 = Line (c, Operation (add, x, y));;
+let blk_0 = [ line_1; line_2; ];;
+let blk_1 = [ line_1; line_2; line_3; line_4 ];;
 
-let hash_block blk = 
-    let seed = ref 0 in
+let hash_block blk seed = 
+    let seed = ref seed in
     let tbl = Hashtbl.create 123456 in 
     let symbol_exists s = 
-        Hashtbl.mem tbl s
-    in
-    let hash_line line = 
-        match line with
-        | Line (Symbol t, Operation (BinaryFunction b, Symbol x, Symbol y)) ->
-            if not (symbol_exists x) then 
-                begin
-                    Hashtbl.add tbl x !seed; 
-                    incr seed
-                end
-            else if not (symbol_exists y) then
-                begin
-                    Hashtbl.add tbl y !seed; 
-                    incr seed
-                end
-            else if not (symbol_exists (b ^ x ^ y)) then
+        Hashtbl.mem tbl s in
+    let hash_line l =
+        match l with
+        | t , b , x , y ->
+            if not (symbol_exists (b ^ x ^ y)) then
                 begin
                     Hashtbl.add tbl (b ^ x ^ y) !seed; 
                     Hashtbl.add tbl t !seed; 
                     incr seed
                 end
             else
-                Hashtbl.add tbl t (Hashtbl.find tbl (b ^ x ^ y))
-        | Line (Symbol _, Symbol _) ->
-                Hashtbl.add tbl "a" 1
-        | Line (_, _) ->
-                Hashtbl.add tbl "a" 1 
+                Hashtbl.add tbl t (Hashtbl.find tbl (b ^ x ^ y)) 
     in
+    let hash_symb s =
+        if not (symbol_exists s) then
+            begin
+                Hashtbl.add tbl s !seed; 
+                incr seed
+            end
+    in
+    let hash l = 
+        match l with
+        | Line (Symbol t, Operation (BinaryFunction b, Symbol x, Symbol y)) ->
+            begin
+                hash_symb x;
+                hash_symb y;
+                hash_line (t, b, x, y)
+            end
+        | Line (Symbol x, Symbol y) ->
+            begin
+                hash_symb y;
+            end
+    in
+    List.iter hash blk;
     tbl;;
 
 let test_harness blk = 
-    hash_block blk;;
+    hash_block blk 1;;
+
+Hashtbl.iter (fun x y -> Printf.printf "%s -> %d\n" x y) (test_harness blk_1);;
